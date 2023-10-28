f76f9a9df2e7fc6cb5f830c77761112b31990cf0
Schwartz <darlanswtz@gmail.com>
2023-10-28 08:27:03 -0300

    ?

--- src/pages/Dashboard/Administration/index.jsx
+++ src/pages/Dashboard/Administration/index.jsx
@@ -56,6 +56,9 @@ export default function AdministrationPage() {
         if(!localStorage.getItem('admin')){
             return navigate('/sign-in');
         }
+        if(!localStorage.getItem('token')){
+            return navigate('/sign-in');
+        }
     }, []);
     useEffect(() => {
         if (event) {
